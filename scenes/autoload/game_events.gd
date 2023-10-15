b043c0e238c622973c99368a5255143b83511dcd
Ivan Valles-Perez <ivanvallesperez@gmail.com>
2023-10-15 22:24:57 +0100

    Add vignette

--- scenes/autoload/game_events.gd
+++ scenes/autoload/game_events.gd
@@ -5,6 +5,7 @@ signal ability_upgrade_added(
 	upgrade: AbilityUpgrade,
 	current_upgrades: Dictionary,
 )
+signal player_damaged
 
 
 func emit_experience_vial_collected(number: float):
@@ -16,3 +17,7 @@ func emit_ability_upgrade_added(
 	current_upgrades: Dictionary,
 ):
 	ability_upgrade_added.emit(upgrade, current_upgrades)
+
+
+func emit_player_damaged():
+	player_damaged.emit()
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -72,6 +72,7 @@ func on_damage_interval_timer_timeout():
 
 
 func on_health_changed():
+	GameEvents.emit_player_damaged()
 	update_healthbar()
 
 
