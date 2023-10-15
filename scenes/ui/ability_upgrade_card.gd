0989a2c38e21912fd09831accd6b25ab9482b025
Ivan Valles-Perez <ivanvallesperez@gmail.com>
2023-10-15 14:50:53 +0100

    Improve upgrade screen visuals

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -2,12 +2,23 @@ extends PanelContainer
 
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+@onready var animation_player = $AnimationPlayer
+@onready var hover_animation_player = $HoverAnimationPlayer
 
 signal selected
+
+var disabled: bool = false
 
 
 func _ready():
 	gui_input.connect(on_card_selected)
+	mouse_entered.connect(on_mouse_entered)
+
+
+func play_in(delay: float = 0):
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	animation_player.play("in")
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
@@ -16,5 +27,15 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 
 
 func on_card_selected(event: InputEvent):
-	if event.is_action_pressed("left_click"):
+	if event.is_action_pressed("left_click") and not disabled:
+		animation_player.play("selected")
+		for card in get_tree().get_nodes_in_group("upgrade_card"):
+			card.disabled = true
+			if card != self:
+				card.animation_player.play("discard")
+		await animation_player.animation_finished
 		selected.emit()
+
+
+func on_mouse_entered():
+	hover_animation_player.play("hover")
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 @export var upgrade_card_scene: PackedScene
 @onready var card_container: HBoxContainer = %CardContainer
+@onready var animation_player = $AnimationPlayer
 signal upgrade_selected(upgrade: AbilityUpgrade)
 
 
@@ -10,14 +11,19 @@ func _ready():
 
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
+	var delay = 0
 	for upgrade in upgrades:
 		var card = upgrade_card_scene.instantiate()
 		card_container.add_child(card)
 		card.set_ability_upgrade(upgrade)
+		card.play_in(delay)
 		card.selected.connect(on_upgrade_selected.bind(upgrade))
+		delay += .3
 
 
 func on_upgrade_selected(upgrade):
 	upgrade_selected.emit(upgrade)
+	animation_player.play("out")
+	await animation_player.animation_finished
 	get_tree().paused = false  # Unpause
 	queue_free()  # Destroy
